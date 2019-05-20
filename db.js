509c11d8a3dfeb9b89dacbcbb732b4b2e1b62011
Tommy LeBlanc <tlcommodore@gmail.com>
2019-05-19 21:37:07 -0500

    Add PostgrSQL persistent storage to store temperatures

--- db.js
+++ db.js
@@ -1,42 +1,50 @@
 /* jshint node: true */
 'use strict';
 
-var sqlite = require('sqlite3').verbose();
-var fs = require('fs');
+const { Pool } = require('pg');
+const connStr = process.env.DATABASE_URL;
+console.log('connStr: ', connStr);
+const pool = new Pool({
+  connectionString: connStr,
+});
 
-var file = './resources/db/temperatures.db';
+// Store queries elsewhere, and refer to them here when needed
+const currentTimeQuery = `SELECT NOW() as now;`
 
-// Keep this in case there is need to dynamically create a db based on the date.
-// This will be useful for recording the beginning of the brewing campaign.
-// var date = new Date();
-// var db_name = String(date.getFullYear() + "_" + (date.getMonth() + 1) + "_" + date.getDate() + ".db");
-// var file = './resources/db/beer_temp_' + db_name;
+const checkTableQuery = `SELECT count(*) as cnt
+                         FROM pg_tables t
+                         WHERE tableowner='serviceaccount' 
+                         AND tablename='temperatures';`
+
+const createTableQuery = `CREATE TABLE IF NOT EXISTS temperatures (
+                              id              SERIAL PRIMARY KEY,
+                              temp_room       real NOT NULL,
+                              temp_ext        real NOT NULL,
+                              ts              timestamp);`
+
+const addTempQuery = `INSERT INTO temperatures(temp_room, temp_ext, ts)
+                      VALUES ($1, $2, $3);`
+
+pool.query(currentTimeQuery)
+      .then(res => console.log(res.rows[0]))
+      .catch(e => console.error(e.stack));
+
+pool.query(createTableQuery)
+      .then(res => console.log("Table present or created."))
+      .catch(e => console.error(e.stack));
 
 exports.postTemperatures = function postTemperatures( tempData, callback ) {
-  // Ensure database file exists
-  var exists = fs.existsSync(file);
-  var db = new sqlite.Database(file);
-
-  db.serialize(function( err, result ){
-    if(!exists) {
-      db.run("CREATE TABLE beer_temp ( `id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, `room` REAL, `external` REAL, `time` TEXT )");
-      if (err) {
-        callback(err, null);
-      }
-    }
-
-    // tempData is in UTC from the monitoring service
-    var stmt = db.prepare("INSERT INTO beer_temp (room, external, time) VALUES (?, ?, ?)");
-    stmt.run(tempData.room, tempData.weather, tempData.time);
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, result);
-    }
-
-    stmt.finalize();
-
-  });
-
-db.close();
+  pool.connect()
+  .then(client => {
+    return client.query(addTempQuery, [tempData.room, tempData.weather, new Date()])
+      .then(res => {
+        client.release()
+        callback(null, tempData)
+      })
+      .catch(e => {
+        console.log("Failure", e.stack)
+        client.release()
+        callback(e.stack, null)
+      })
+  })
 };
